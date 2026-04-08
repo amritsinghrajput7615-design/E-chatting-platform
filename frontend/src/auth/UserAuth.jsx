1f1b694946ad9a2f365a2a7496ca540c73a4409b
Amrit Kumar <amritsinghrajput7615@gmail.com>
2026-04-09 00:11:13 +0530

    fix: render deployment

--- frontend/src/auth/UserAuth.jsx
+++ frontend/src/auth/UserAuth.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { UserContext } from '../context/user.context'
+import { UserContext } from '../context/User.context.jsx'
 import axios from '../config/axios'
 
 const UserAuth = ({ children }) => {
